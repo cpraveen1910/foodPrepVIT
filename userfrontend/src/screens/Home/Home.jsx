89ede8b585815ff94b63ef5f52f131a45b213b9c
cpraveen1910 <ppraveen5019@gmail.com>
2025-07-01 16:40:03 +0530

    Update Home.jsx

--- userfrontend/src/screens/Home/Home.jsx
+++ userfrontend/src/screens/Home/Home.jsx
@@ -6,11 +6,14 @@ import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
   const [category, setCategory]= useState("All")
+
   return (
     <div className='home'>
-      <Header/>
-      <ExploreMenu category={category} setCategory={setCategory}/>
-      <FoodDisplay category={category}/>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <div id="food-display">
+        <FoodDisplay category={category} />
+      </div>
     </div>
   )
 }
